1d93d12d95ca6ffc1e5e72cb52d0d6e76a9d9073
Navas <navasmuller01@gmail.com>
2022-10-24 16:14:17 +0300

    practice

---
+++ app/src/main/java/com/example/navashub/activities/SignInActivity.java
@@ -0,0 +1,16 @@
+package com.example.navashub.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.navashub.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
